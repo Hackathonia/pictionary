94d831d4ce97684f7a1a3d78157c3df306500a7a
Cathryn G <cgorlinsky@gmail.com>
2014-05-24 20:02:10 -0400

    Add player class.

--- server/pictionary_server.py
+++ server/pictionary_server.py
@@ -1,7 +1,10 @@
 import random
 import linecache
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
 
-class Word_Generator():
+class Word_Generator:
 
     filename = "word_list.txt"
 
@@ -15,3 +18,55 @@ class Word_Generator():
         line = linecache.getline(filename, random.randint(0, file_len(filename)))
         return line
 
+    word = random_file_line(filename)
+
+class Player:
+
+    def __init__(self, nickname, win_count):
+        self.nickname = nickname
+        self.win_count = win_count
+
+class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
+    def open(self, *args):
+        print("open", "WebSocketChatHandler")
+        clients.append(self)
+
+    def on_message(self, message):
+        print(message)
+        for client in clients:
+            client.write_message(message)
+
+    def on_close(self):
+        clients.remove(self)
+
+
+
+
+
+
+# from example code for handling chat via websocket
+
+# clients = []
+
+# class IndexHandler(tornado.web.RequestHandler):
+#     @tornado.web.asynchronous
+#     def get(request):
+#         request.render("index.html")
+
+# class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
+#     def open(self, *args):
+#         print("open", "WebSocketChatHandler")
+#         clients.append(self)
+
+#     def on_message(self, message):
+#         print(message)
+#         for client in clients:
+#             client.write_message(message)
+
+#     def on_close(self):
+#         clients.remove(self)
+
+# app = tornado.web.Application([(r'/chat', WebSocketChatHandler), (r'/', IndexHandler)])
+
+# app.listen(8000)
+# tornado.ioloop.IOLoop.instance().start()
